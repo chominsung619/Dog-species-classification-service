c4fd70778ab5c684cf67ba958c10731d93f8ab7c
chominsung619 <20242229@sdhs.gwe.hs.kr>
2024-12-12 11:53:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ st.markdown("""
 content_data = {
     labels[0]: {
         'images': [
-            "https://ibb.co/z57ZW2W"
+            "https://ibb.co/WpmyQyW"
             "https://via.placeholder.com/300?text=Label1_Image2",
             "https://via.placeholder.com/300?text=Label1_Image3"
         ],
